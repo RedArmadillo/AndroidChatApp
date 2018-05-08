c5237abec3bd9576268e3f14fbf27aa1893b1bde
Trinh <trinhpn@uw.edu>
2018-05-08 01:36:06 -0700

    Polling from chat list activity

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatListActivity.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatListActivity.java
@@ -1,5 +1,7 @@
 package group7.tcss450.uw.edu.chatapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,12 +24,14 @@ import java.util.List;
 import group7.tcss450.uw.edu.chatapp.Async.SendPostAsyncTask;
 import group7.tcss450.uw.edu.chatapp.Models.ChatRoom;
 import group7.tcss450.uw.edu.chatapp.Utils.ChatRoomViewAdapter;
+import group7.tcss450.uw.edu.chatapp.Utils.ListenManager;
 
 public class ChatListActivity extends AppCompatActivity {
     private List<ChatRoom> chatRoomList = new ArrayList<>();
     private RecyclerView recyclerView;
     private ChatRoomViewAdapter mAdapter;
     private String mCreateRoomURL;
+    private ListenManager mListenManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,12 +54,11 @@ public class ChatListActivity extends AppCompatActivity {
             }
         });
         fab.setOnClickListener(this::createNewRoom);
-        loadRooms("");
+        //loadRooms("");
     }
 
     public void createNewRoom(View view) {
         JSONObject messageJson = new JSONObject();
-
 
         try {
             //InputMethodManager imm = (InputMethodManager) getContext().getSystemService(INPUT_METHOD_SERVICE);
@@ -65,25 +69,25 @@ public class ChatListActivity extends AppCompatActivity {
         }
 
         new SendPostAsyncTask.Builder(mCreateRoomURL, messageJson)
-                .onPostExecute(this::updateRoom)
+                .onPostExecute(this::endOfCreateRoom)
                 .onCancelled(this::handleError)
                 .build().execute();
     }
 
-    private void updateRoom(String result) {
+    private void endOfCreateRoom(String result) {
         try {
             JSONObject res = new JSONObject(result);
-            Log.d("response from service", result);
+            Log.d("response from service for create new room", result);
 
             if(res.get(getString(R.string.keys_json_success)).toString()
                     .equals(getString(R.string.keys_json_success_value_true))) {
-                int newChatID = res.getInt(getString(R.string.keys_json_new_chat_id));
-                chatRoomList.add(new ChatRoom(newChatID, "new room", "hello", "me"));
-                mAdapter.notifyDataSetChanged();
-               recyclerView.smoothScrollToPosition(0);
+                //int newChatID = res.getInt(getString(R.string.keys_json_new_chat_id));
+                //chatRoomList.add(new ChatRoom(newChatID, "new room", "hello", "me"));
+                //mAdapter.notifyDataSetChanged();
+               //recyclerView.smoothScrollToPosition(0);
             }
         } catch (JSONException e) {
-            Log.d("endOfSend", "error");
+            Log.d("endOfUpdateNewRoom", "error");
             e.printStackTrace();
         }
     }
@@ -96,15 +100,59 @@ public class ChatListActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-
+        SharedPreferences prefs =
+                getSharedPreferences(getString(R.string.keys_shared_prefs),
+                        Context.MODE_PRIVATE);
+        String currentUser = prefs.getString(getString(R.string.keys_prefs_username), "");
         mCreateRoomURL = new Uri.Builder()
                 .scheme("https")
                 .authority(getString(R.string.ep_lab_url))
                 .appendPath(getString(R.string.ep_create_room))
+                .appendQueryParameter("username", currentUser)
                 .build()
                 .toString();
+
+
+        Uri retrieve = new Uri.Builder()
+                .scheme("https")
+                .authority(getString(R.string.ep_lab_url))
+                .appendPath(getString(R.string.ep_get_rooms))
+                .appendQueryParameter("username", currentUser)
+                .build();
+
+        mListenManager = new ListenManager.Builder(retrieve.toString(),
+                this::publishProgress)
+                .setExceptionHandler(this::handleError)
+                .setDelay(100000)
+                .build();
     }
 
+    private void publishProgress(JSONObject chatids) {
+        //final String[] ids;
+        if(chatids.has(getString(R.string.keys_json_chat_id))) {
+            Log.d("Success", "Received list of room....");
+            try {
+                JSONArray jIds = chatids.getJSONArray(getString(R.string.keys_json_chat_id));
+                for (int i = 0; i < jIds.length(); i++) {
+                    JSONObject id = jIds.getJSONObject(i);
+                    int roomId = id.getInt(getString(R.string.keys_json_chat_id_lowercase));
+                    Log.d("id: " , Integer.toString(roomId));
+                    ChatRoom c = new ChatRoom(roomId);
+                    chatRoomList.add(c);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return;
+            }
+            mAdapter.notifyDataSetChanged();
+        } else {
+            Log.d("err", "received no room!!!");
+        }
+    }
+
+    private void handleError(Exception e) {
+        Log.e("Error in listen", e.toString());
+    }
 
 
     private void loadRooms(String result) {
@@ -118,4 +166,11 @@ public class ChatListActivity extends AppCompatActivity {
         chatRoomList.add(four);
         mAdapter.notifyDataSetChanged();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mListenManager.startListening();
+    }
+
 }
