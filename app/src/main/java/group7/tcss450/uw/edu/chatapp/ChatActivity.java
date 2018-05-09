cc9045849b6349e04c34261dc365ac53efb8d89e
Trinh <trinhpn@uw.edu>
2018-05-09 14:34:09 -0700

    Add Divider in Chat List for Recent and Invitation

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatActivity.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatActivity.java
@@ -1,17 +1,16 @@
 package group7.tcss450.uw.edu.chatapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import group7.tcss450.uw.edu.chatapp.Fragment.ChatFragment;
+import group7.tcss450.uw.edu.chatapp.Utils.SettingMenuActivity;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends SettingMenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
 
         if (savedInstanceState == null) {
             //if (findViewById(R.id.chatLayout) != null) {
