3c2dde34e081db722ec7f7b0c01e171a50aea1ca
Trinh <trinhpn@uw.edu>
2018-05-09 02:40:14 -0700

    Save memberid in prefs

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatActivity.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/ChatActivity.java
@@ -1,8 +1,6 @@
 package group7.tcss450.uw.edu.chatapp;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import group7.tcss450.uw.edu.chatapp.Fragment.ChatFragment;
@@ -13,12 +11,14 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager
-                .beginTransaction()
-                .replace(R.id.chatLayout, new ChatFragment());
 
-        // Commit the transaction
-        transaction.commit();
+
+        if (savedInstanceState == null) {
+            if (findViewById(R.id.chatLayout) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.chatLayout, new ChatFragment())
+                        .commit();
+            }
+        }
     }
 }
