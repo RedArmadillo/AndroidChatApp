d2728ee2d7341401833a6603ef7d1e47d87880c7
blackarro18 <vampxhelix@gmail.com>
2018-05-15 11:03:32 -0700

    Minor Fixes

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/Activities/MainActivity.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/Activities/MainActivity.java
@@ -9,13 +9,15 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ListView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import group7.tcss450.uw.edu.chatapp.Async.SendPostAsyncTask;
-import group7.tcss450.uw.edu.chatapp.Fragment.HomeFragment;
 import group7.tcss450.uw.edu.chatapp.Fragment.ReSendEmailFragment;
 import group7.tcss450.uw.edu.chatapp.Front_End_Register_Login.Credentials;
 import group7.tcss450.uw.edu.chatapp.Front_End_Register_Login.LoginFragment;
@@ -264,6 +266,20 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
     }
 
 
+    public static class ThemeChangeActivity extends AppCompatActivity implements View.OnClickListener{
+        ListView mListView;
+        Button mChangeTheme;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_theme_change);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
 }
 
 
