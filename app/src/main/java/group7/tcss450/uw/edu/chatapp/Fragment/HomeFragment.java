8b23672ad3b931ec6ac23e0770637de113ee52ce
blackarro18 <vampxhelix@gmail.com>
2018-05-22 11:10:21 -0700

    HomeMenu

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/Fragment/HomeFragment.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/Fragment/HomeFragment.java
@@ -3,15 +3,12 @@ package group7.tcss450.uw.edu.chatapp.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import group7.tcss450.uw.edu.chatapp.Activities.ChatListActivity;
-import group7.tcss450.uw.edu.chatapp.Activities.Connections.CurrentConnections;
-import group7.tcss450.uw.edu.chatapp.Models.ChatRoom;
 import group7.tcss450.uw.edu.chatapp.R;
 import group7.tcss450.uw.edu.chatapp.Utils.SettingMenuActivity;
 
@@ -38,47 +35,20 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         weather = v.findViewById(R.id.weatherButton);
-        chat = v.findViewById(R.id.chatButton);
+        chat = v.findViewById(R.id.chatButton2);
         Chatlist = v.findViewById(R.id.chatRoomButton);
-        settings = v.findViewById(R.id.settingButton);
-        Log.d("I am infact listenting ", "I AM LISTNEING");
+        settings = v.findViewById(R.id.settingButton2);
         weather.setOnClickListener(
                 view -> getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.FLAY, new WeatherFragment()).addToBackStack(null)
-                .commit());
+                        .beginTransaction()
+                        .replace(R.id.FLAY, new WeatherFragment()).addToBackStack(null)
+                        .commit());
 
         chat.setOnClickListener(view -> getFragmentManager()
                 .beginTransaction()
@@ -97,44 +67,6 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-/*
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnSuccessFragmentInteractionListener) {
-            mListener = (OnSuccessFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnRegisterFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    } */
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-
-    public interface OnSuccessFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
+
