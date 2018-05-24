0f455bdac2c615a581ef5def987cdb23e761cd06
Alyssa I <alyssa@cyberwit.ch>
2018-05-23 23:37:25 -0700

    Rewind

--- app/src/main/java/group7/tcss450/uw/edu/chatapp/Fragment/WeatherFragment.java
+++ app/src/main/java/group7/tcss450/uw/edu/chatapp/Fragment/WeatherFragment.java
@@ -1,8 +1,12 @@
 package group7.tcss450.uw.edu.chatapp.Fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,16 +36,9 @@ import group7.tcss450.uw.edu.chatapp.R;
  * create an instance of this fragment.
  */
 public class WeatherFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -47,36 +48,29 @@ public class WeatherFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment WeatherFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static WeatherFragment newInstance(String param1, String param2) {
         WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
+        
         View v = inflater.inflate(R.layout.fragment_weather, container, false);
         v.findViewById(R.id.btnWeatherZip).setOnClickListener(this::getWeatherByZip);
+        v.findViewById(R.id.btnWeatherLoc).setOnClickListener(this::getWeatherByCurrentLoc);
         return v;
     }
 
@@ -150,6 +144,96 @@ public class WeatherFragment extends Fragment {
     }
 
     private void endOfWeatherByZipTask(final String result) {
+        Log.d("response from weather service", result);
+
+        try {
+            JSONArray data = new JSONObject(result).getJSONArray("data");
+            JSONObject res = data.getJSONObject(0);
+            String currTemp = res.getString("temp");
+            String maxTemp = res.getString("max_temp");
+            String minTemp = res.getString("min_temp");
+            String desc = res.getJSONObject("weather").getString("description");
+
+            ((TextView) getView().findViewById(R.id.tvWeatherTemp)).setText(currTemp);
+            ((TextView) getView().findViewById(R.id.tvWeatherHighLow)).setText(
+                    maxTemp + " / " + minTemp
+            );
+            ((TextView) getView().findViewById(R.id.tvWeatherCond)).setText(desc);
+
+        } catch (JSONException e) {
+            Log.d("endOfSend", "error");
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * Takes a zip code from the weather fragment zipcode EditText box and sends a GET request
+     * to our weather service, setting the appropriate TextViews to the requested weather data.
+     *
+     * @param theButton The get weather by zipcode button
+     */
+    private void getWeatherByCurrentLoc(final View theButton) {
+
+        class Cont {
+            Location mLoc;
+
+            void setLoc(Location theLoc) {
+                mLoc = theLoc;
+            }
+
+            Location getLoc() {
+                return mLoc;
+            }
+        }
+
+        Cont cont = new Cont();
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(),
+                    new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                cont.setLoc(location);
+                                Log.d("Weather by current loc",
+                                        Double.toString(location.getLatitude())
+                                        + " " + Double.toString(location.getLatitude())
+                                );
+
+                            } else {
+                                Log.d("Weather by current loc", "last location is null");
+                            }
+                        }
+                    });
+
+            Location loc = cont.getLoc();
+            String lat = Double.toString(loc.getLatitude());
+            String lon = Double.toString(loc.getLongitude());
+
+            Uri uri10Day = new Uri.Builder()
+                    .scheme("https")
+                    .authority(getString(R.string.ep_weather_base_url))
+                    .appendPath(getString(R.string.ep_weather_v2_0))
+                    .appendPath(getString(R.string.ep_weather_forecast))
+                    .appendPath(getString(R.string.ep_weather_daily))
+                    .appendQueryParameter(getString(R.string.param_weather_lat), lat)
+                    .appendQueryParameter(getString(R.string.param_weather_lon), lon)
+                    .appendQueryParameter(getString(R.string.param_weather_units), "I")
+                    .appendQueryParameter(getString(R.string.param_weather_api),
+                            getString(R.string.keys_weather_API))
+                    .build();
+
+            new SendGetAsyncTask.Builder(uri10Day.toString(), new JSONObject())
+                    .onPostExecute(this::endOfWeatherByZipTask)
+                    .onCancelled(this::handleError)
+                    .build().execute();
+        }
+
+
+    }
+
+    private void endOfWeatherByCurrentLocTask(final String result) {
         Log.d("response from weather service", result);
 
         try {
